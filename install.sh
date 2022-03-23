80e940eb9cec00b7cb075a5e8c1446d728d0602a
XiongHui Guo <gz_shines@msn.com>
2022-03-24 05:38:45 +0800

    install.sh output format
    Signed-off-by: XiongHui Guo <gz_shines@msn.com>

--- install.sh
+++ install.sh
@@ -32,7 +32,11 @@ if [ -d "${Install_To_Folder}" ]; then
 
 	alias open-vpn-cmd='${Install_To_Folder}/open-vpn-cmd.sh'
 
+	echo "----------------------------------------------------------------------------------"
 	echo "[vpn-shell-for-openconnect]: Install success. already install to ${Install_To_Folder} folder."
+	echo "----------------------------------------------------------------------------------"
 else
+	echo "----------------------------------------------------------------------------------"
 	echo "[vpn-shell-for-openconnect]: Install failed, ${Install_To_Folder} folder is not existed."
+	echo "----------------------------------------------------------------------------------"
 fi
